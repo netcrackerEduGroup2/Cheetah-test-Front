eb8908dc71fc175f23b3a820aa6bb82c317f7486
Kotyara <babanina.ev@gmail.com>
2020-12-27 13:46:07 +0200

    remove console.log()

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -67,7 +67,6 @@ export class SignUpComponent implements OnInit, OnDestroy {
       .pipe(first())
       .subscribe({
           next: data => {
-            console.log(data);
             switch (data) {
               case environment.success: {
                 this.user = new User();
